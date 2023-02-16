68d6d561eeaa13aa688460556eb1c6d67115047b
thebiebs <blavoprince@gmail.com>
2023-02-16 10:31:21 -0800

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ * Return: 1 if success
+ */
+int main(void)
+{
+	fput(stdout, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
