f6aa38fd8b405edcc4455f33179cb884aa07fe18
thebiebs <blavoprince@gmail.com>
2023-02-16 10:33:25 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,7 +5,8 @@
  * Return: 1 if success
  */
 int main(void)
-{
-	fput(stdout, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-	return (1);
+	{
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
+return (1);
 }
