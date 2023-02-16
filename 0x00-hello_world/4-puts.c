216d063ba1500fa9b43ad01e91737cfde84b7066
thebiebs <blavoprince@gmail.com>
2023-02-16 08:03:49 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Print out a string without the use of printf.
+ * Return: 0 if success
+ */
+int main(void)
+	{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+	}
