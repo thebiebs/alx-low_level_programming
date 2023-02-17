656673ea0db256018080b4185a0235c8cc8418d0
thebiebs <blavoprince@gmail.com>
2023-02-17 06:24:58 -0800

    0-positive_or_negative.c

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes here */
+
+/* Betty styling goes here*/
+int main(void)
+{
+	int n;
+
+	srand(time(0)0;
+	n = rand() - RAND_MAX / 2;
+	return (0);
+	}
